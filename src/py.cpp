b348ff3180912160d1d9bf37a527b439d574cb2a
Wush Wu <wush978@gmail.com>
2013-04-18 01:07:24 +0800

    finisy pycall

--- src/py.cpp
+++ src/py.cpp
@@ -8,34 +8,48 @@ RcppExport SEXP RembedPy__pyscript(SEXP Rscript) {
   END_REMBEDPY
 }
 
-std::vector< PyObjPtr > extract_argv(SEXP Rargv) {
+boost::python::list extract_argv(SEXP Rargv) {
+#ifdef REMBEDPY_DEBUG
+	Rprintf("extract_argv\n");
+#endif
 	Rcpp::List argv(Rargv);
-	std::vector< PyObjPtr > retval;
+	boost::python::list retval;
 	for(int i = 0;i < argv.size();i++) {
-		retval.push_back( PyObjPtr(Rcpp::wrap(argv[i])) );
+		PyObjPtr glue(Rcpp::wrap(Rcpp::S4(Rcpp::wrap(argv[i])).slot("ptr")));
+		retval.append(*glue);
 	}
 	return retval;
 }
 
-SEXP pycall(boost::python::object& callable, std::vector< PyObjPtr >& argv) {
-	return R_NilValue;
+SEXP pycall(boost::python::object& callable, boost::python::list& argv) {
+#ifdef REMBEDPY_DEBUG
+	Rprintf("pycall\n");
+#endif
+	PyObjPtr retval(new boost::python::object(callable(*boost::python::tuple(argv))));
+	return Rcpp::wrap(retval);
 }
 
 SEXP pycall__funname(SEXP Rmodule_name, SEXP Rfun_name, SEXP Rargv) {
+#ifdef REMBEDPY_DEBUG
+	Rprintf("funname\n");
+#endif
 	std::string 
 		fun_name(Rcpp::as<std::string>(Rfun_name)),
 		module_name(Rcpp::as<std::string>(Rmodule_name));
   boost::python::object module((boost::python::handle<>(boost::python::borrowed(PyImport_AddModule(module_name.c_str())))));
 	boost::python::object callable(module.attr("__dict__")[fun_name.c_str()]);
-	std::vector< PyObjPtr > argv(extract_argv(Rargv));
+	boost::python::list argv(extract_argv(Rargv));
 	return pycall(callable, argv);
 }
 
 SEXP pycall__callable(SEXP Rfun, SEXP Rargv) {
+#ifdef REMBEDPY_DEBUG
+	Rprintf("callable\n");
+#endif
 	Rcpp::S4 fun(Rfun);
 	PyObjPtr pcallable(Rcpp::wrap(fun.slot("ptr")));
 	boost::python::object& callable(*pcallable);
-	std::vector< PyObjPtr > argv(extract_argv(Rargv));
+	boost::python::list argv(extract_argv(Rargv));
 	return pycall(callable, argv);
 }
 
