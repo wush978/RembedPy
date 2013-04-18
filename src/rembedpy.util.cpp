d0c83b41c57f0c2820fba658ad3a512c3f7baccb
Wush Wu <wush978@gmail.com>
2013-04-18 13:41:58 +0800

    add debug messages

--- src/rembedpy.util.cpp
+++ src/rembedpy.util.cpp
@@ -3,13 +3,13 @@
 namespace RembedPy {
   
   boost::python::list extract_argv_list(SEXP Rargv) {
-  #ifdef REMBEDPY_DEBUG
+#ifdef REMBEDPY_DEBUG
     Rprintf("extract_argv_list: ");
-  #endif
+#endif
     Rcpp::List argv(Rargv);
-  #ifdef REMBEDPY_DEBUG
+#ifdef REMBEDPY_DEBUG
     Rprintf("length(Rargv)=%d \n", argv.size());
-  #endif
+#endif
   	boost::python::list retval;
   	for(int i = 0;i < argv.size();i++) {
   		PyObjPtr glue(Rcpp::wrap(Rcpp::S4(Rcpp::wrap(argv[i])).slot("ptr")));
@@ -19,13 +19,13 @@ namespace RembedPy {
   }
   
   boost::python::dict extract_argv_dict(SEXP Rargv) {
-  #ifdef REMBEDPY_DEBUG
+#ifdef REMBEDPY_DEBUG
     Rprintf("extract_argv_dict: ");
-  #endif
+#endif
     Rcpp::List argv(Rargv);
-  #ifdef REMBEDPY_DEBUG
+#ifdef REMBEDPY_DEBUG
     Rprintf("length(Rargv)=%d \n", argv.size());
-  #endif
+#endif
   	boost::python::dict retval;
     if (argv.size() == 0) return retval;
     std::vector<std::string> argv_name(Rcpp::as< std::vector<std::string> >(Rcpp::wrap(argv.attr("names"))));
@@ -37,14 +37,17 @@ namespace RembedPy {
   }
   
   SEXP pycall(boost::python::object& callable, boost::python::list& argv_list, boost::python::dict& argv_dict) {
-  #ifdef REMBEDPY_DEBUG
+#ifdef REMBEDPY_DEBUG
     Rprintf("pycall\n");
-  #endif
+#endif
   	PyObjPtr retval(new boost::python::object(callable(*boost::python::tuple(argv_list), **argv_dict)));
   	return Rcpp::wrap(retval);
   }
   
   boost::python::object extract_pyobj(std::string& name, std::string& module) {
+#ifdef REMBEDPY_DEBUG
+    Rprintf("extract_pyobj %s from module: %s \n", name.c_str(), module.c_str());
+#endif 
     boost::python::object m((boost::python::handle<>(boost::python::borrowed(PyImport_AddModule(module.c_str())))));
     boost::python::object retval(m.attr("__dict__")[name.c_str()]);
     return retval;
